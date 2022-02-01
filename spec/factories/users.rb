89d1601ee94ed1ff6b36075423daa7441f495a27
shotaro0522 <68640527+shotaro0522@users.noreply.github.com>
2022-02-01 16:17:37 +0900

    Fakerに漢字とカタカナを追加

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,13 +1,19 @@
 FactoryBot.define do
+
   factory :user do
+
+    transient do
+      person { Gimei.name }
+    end
+
     nickname              { Faker::Name.initials(number: 2) }
     email                 { Faker::Internet.free_email }
     password              { '1a' + Faker::Internet.password(min_length: 4) }
     password_confirmation { password }
-    last_name        { Faker::Name.last_name }
-    first_name       { Faker::Name.first_name }
-    last_name_kana { last_name }
-    first_name_kana { first_name }
+    last_name        { person.last.kanji }
+    first_name       { person.first.kanji }
+    last_name_kana { person.last.katakana }
+    first_name_kana { person.first.katakana }
     birthday { Faker::Date }
   end
 end
