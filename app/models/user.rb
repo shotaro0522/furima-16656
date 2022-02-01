ddbbe087e566611109ab97b9fea54d8bbe9b6925
shotaro0522 <68640527+shotaro0522@users.noreply.github.com>
2022-02-01 19:21:22 +0900

    【修正】バリデーションの修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,10 +11,10 @@ class User < ApplicationRecord
   validates :first_name_kana, presence: true
   validates :birthday, presence: true
 
-  VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze
+  VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
   validates :password, format: { with: VALID_PASSWORD_REGEX, message: 'は半角英数を両方含む必要があります' }
 
-  VALID_NAME_REGEX = /\A[ぁ-んァ-ン一-龥]/.freeze
+  VALID_NAME_REGEX = /\A[ぁ-んァ-ヶ一-龥々ー]+\z/.freeze
   validates :last_name, format: { with: VALID_NAME_REGEX, message: 'は全角日本語（かな、カナ、漢字のみ）である必要があります' }
   validates :first_name, format: { with: VALID_NAME_REGEX, message: 'は全角日本語（かな、カナ、漢字のみ）である必要があります' }
 
