3252060ce663587deb5463a8d097880c5b8f69b0
shotaro0522 <68640527+shotaro0522@users.noreply.github.com>
2022-02-02 17:08:50 +0900

    商品登録のテストコード作成

--- app/models/item.rb
+++ app/models/item.rb
@@ -5,8 +5,8 @@ class Item < ApplicationRecord
   belongs_to :user
   has_one_attached :image
 
-  validates :name, :info, :price, :image, presence: true
-  validates :category_id, :state_id, :delivery_charge_id, :prefecture_id, :delivery_date_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :name, :info, :price, :image, :category_id, :state_id, :delivery_charge_id, :prefecture_id, :delivery_date_id, presence: true
+  validates :category_id, :state_id, :delivery_charge_id, :prefecture_id, :delivery_date_id, numericality: { other_than: 1 }
 
   validates :price, numericality: {only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999}
 
--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,25 @@
 FactoryBot.define do
   factory :item do
-    
+
+    transient do
+      person { Gimei.name }
+    end
+
+    name {person.first.katakana}
+    info {Faker::Lorem.sentence}
+    category_id {Faker::Number.between(from: 2, to: 11)}
+    state_id{Faker::Number.between(from: 2, to: 7)}
+    delivery_charge_id{Faker::Number.between(from: 2, to: 3)}
+    prefecture_id{Faker::Number.between(from: 2, to: 48)}
+    delivery_date_id{Faker::Number.between(from: 2, to: 4)}
+    price{Faker::Number.between(from: 300, to: 9999999)}
+
+    association :user
+
+    after(:build) do |item|
+      item.image.attach(io: File.open('public/images/test_image.png'), filename: 'test_image.png')
+    end
+
+
   end
 end
