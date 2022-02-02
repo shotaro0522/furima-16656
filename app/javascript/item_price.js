044a08dc35380dce1c695ee9fb5f3950b65ce54a
shotaro0522 <68640527+shotaro0522@users.noreply.github.com>
2022-02-02 19:00:00 +0900

    【修正】計算誤差の修正

--- app/javascript/item_price.js
+++ app/javascript/item_price.js
@@ -2,13 +2,14 @@ function calculation(){
   const priceInput = document.getElementById("item-price");
 
   priceInput.addEventListener("input", () => {
+    inputValue = priceInput.value
 
     const addTaxDom = document.getElementById("add-tax-price");
     tax = inputValue*0.1
     addTaxDom.innerHTML = (Math.floor(tax))
 
     const profit = document.getElementById("profit");
-    profit.innerHTML = (Math.floor(inputValue - tax))
+    profit.innerHTML = (Math.floor(inputValue - Math.floor(tax)))
   })
 }
 window.addEventListener('load', calculation)
