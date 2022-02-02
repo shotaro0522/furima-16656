c85a0d74f98e9c7715800124367d4e723babb06b
shotaro0522 <68640527+shotaro0522@users.noreply.github.com>
2022-02-02 17:39:10 +0900

    Rubocopによる修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,4 @@
 class ItemsController < ApplicationController
-
   before_action :authenticate_user!, only: [:new]
 
   def index
@@ -21,9 +20,7 @@ class ItemsController < ApplicationController
   private
 
   def item_params
-    params.require(:item).permit(:name, :info, :price, :image, :category_id, :state_id, :delivery_charge_id, :prefecture_id, :delivery_date_id).merge(user_id: current_user.id)
+    params.require(:item).permit(:name, :info, :price, :image, :category_id, :state_id, :delivery_charge_id, :prefecture_id,
+                                 :delivery_date_id).merge(user_id: current_user.id)
   end
-  
-  
-
 end
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -7,120 +7,113 @@ RSpec.describe Item, type: :model do
 
   describe '商品の新規登録' do
     context '新規登録がうまくいくとき' do
-
-      it "必須項目が全て入力されていれば登録できる" do
+      it '必須項目が全て入力されていれば登録できる' do
         expect(@item).to be_valid
       end
-
     end
 
-
     context '新規登録がうまくいかないとき' do
-
-      it "画像が空" do
+      it '画像が空' do
         @item.image = nil
         @item.valid?
         expect(@item.errors.full_messages).to include("Image can't be blank")
       end
 
-      it "商品名が空" do
+      it '商品名が空' do
         @item.name = ''
         @item.valid?
         expect(@item.errors.full_messages).to include("Name can't be blank")
       end
-      
-      it "商品説明が空" do
-        @item.info= ''
+
+      it '商品説明が空' do
+        @item.info = ''
         @item.valid?
         expect(@item.errors.full_messages).to include("Info can't be blank")
       end
 
-      it "カテゴリーが空" do
-        @item.category_id= ''
+      it 'カテゴリーが空' do
+        @item.category_id = ''
         @item.valid?
         expect(@item.errors.full_messages).to include("Category can't be blank")
       end
 
-      it "カテゴリーのidが1" do
-        @item.category_id= '1'
+      it 'カテゴリーのidが1' do
+        @item.category_id = '1'
         @item.valid?
-        expect(@item.errors.full_messages).to include("Category must be other than 1")
+        expect(@item.errors.full_messages).to include('Category must be other than 1')
       end
 
-      it "状態が空" do
-        @item.state_id= ''
+      it '状態が空' do
+        @item.state_id = ''
         @item.valid?
         expect(@item.errors.full_messages).to include("State can't be blank")
       end
 
-      it "状態のidが1" do
-        @item.state_id= '1'
+      it '状態のidが1' do
+        @item.state_id = '1'
         @item.valid?
-        expect(@item.errors.full_messages).to include("State must be other than 1")
+        expect(@item.errors.full_messages).to include('State must be other than 1')
       end
 
-      it "配達料の負担が空" do
-        @item.delivery_charge_id= ''
+      it '配達料の負担が空' do
+        @item.delivery_charge_id = ''
         @item.valid?
         expect(@item.errors.full_messages).to include("Delivery charge can't be blank")
       end
 
-      it "配達料の負担のidが1" do
-        @item.delivery_charge_id= '1'
+      it '配達料の負担のidが1' do
+        @item.delivery_charge_id = '1'
         @item.valid?
-        expect(@item.errors.full_messages).to include("Delivery charge must be other than 1")
+        expect(@item.errors.full_messages).to include('Delivery charge must be other than 1')
       end
 
-      it "都道府県が空" do
-        @item.prefecture_id= ''
+      it '都道府県が空' do
+        @item.prefecture_id = ''
         @item.valid?
         expect(@item.errors.full_messages).to include("Prefecture can't be blank")
       end
 
-      it "都道府県のidが1" do
-        @item.prefecture_id= '1'
+      it '都道府県のidが1' do
+        @item.prefecture_id = '1'
         @item.valid?
-        expect(@item.errors.full_messages).to include("Prefecture must be other than 1")
+        expect(@item.errors.full_messages).to include('Prefecture must be other than 1')
       end
 
-      it "配達日数が空" do
-        @item.delivery_date_id= ''
+      it '配達日数が空' do
+        @item.delivery_date_id = ''
         @item.valid?
         expect(@item.errors.full_messages).to include("Delivery date can't be blank")
       end
 
-      it "配達日数のidが1" do
-        @item.delivery_date_id= '1'
+      it '配達日数のidが1' do
+        @item.delivery_date_id = '1'
         @item.valid?
-        expect(@item.errors.full_messages).to include("Delivery date must be other than 1")
+        expect(@item.errors.full_messages).to include('Delivery date must be other than 1')
       end
 
-      it "値段が空" do
-        @item.price= ''
+      it '値段が空' do
+        @item.price = ''
         @item.valid?
         expect(@item.errors.full_messages).to include("Price can't be blank")
       end
 
-      it "値段が300より少ない" do
-        @item.price= '10'
+      it '値段が300より少ない' do
+        @item.price = '10'
         @item.valid?
-        expect(@item.errors.full_messages).to include("Price must be greater than or equal to 300")
+        expect(@item.errors.full_messages).to include('Price must be greater than or equal to 300')
       end
 
-      it "値段が9999999より多い" do
-        @item.price= '99999990'
+      it '値段が9999999より多い' do
+        @item.price = '99999990'
         @item.valid?
-        expect(@item.errors.full_messages).to include("Price must be less than or equal to 9999999")
+        expect(@item.errors.full_messages).to include('Price must be less than or equal to 9999999')
       end
 
-      it "値段が半角数値でない" do
-        @item.price= 'あああ'
+      it '値段が半角数値でない' do
+        @item.price = 'あああ'
         @item.valid?
-        expect(@item.errors.full_messages).to include("Price is not a number")
+        expect(@item.errors.full_messages).to include('Price is not a number')
       end
-
     end
-
   end
-
 end
