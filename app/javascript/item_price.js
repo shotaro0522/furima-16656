09a54a922c9c03466d4652d0c557bda5fedd8c2b
shotaro0522 <68640527+shotaro0522@users.noreply.github.com>
2022-02-02 18:02:46 +0900

    不要なコンソールログの削除

--- app/javascript/item_price.js
+++ app/javascript/item_price.js
@@ -2,8 +2,6 @@ function calculation(){
   const priceInput = document.getElementById("item-price");
 
   priceInput.addEventListener("input", () => {
-    console.log("イベント発火");
-    const inputValue = priceInput.value;
 
     const addTaxDom = document.getElementById("add-tax-price");
     tax = inputValue*0.1
@@ -11,24 +9,6 @@ function calculation(){
 
     const profit = document.getElementById("profit");
     profit.innerHTML = (Math.floor(inputValue - tax))
-
   })
-
-
-
-
-
-
-
-
-
-
-
-
-
 }
-
-
-
-
 window.addEventListener('load', calculation)
