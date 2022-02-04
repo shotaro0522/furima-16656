388886a115b88659efc998b90ad6e7b8c7ad4c2e
shotaro0522 <68640527+shotaro0522@users.noreply.github.com>
2022-02-04 21:30:44 +0900

    rubocopによる修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -39,7 +39,7 @@ class ItemsController < ApplicationController
       @item.destroy
       redirect_to root_path
     else
-      redirect_to root_path 
+      redirect_to root_path
     end
   end
 
