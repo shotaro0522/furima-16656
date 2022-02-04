5b756f17696b4849e0e86cf2db2ac8fc5cbdfcdb
shotaro0522 <68640527+shotaro0522@users.noreply.github.com>
2022-02-04 11:53:37 +0900

    商品一覧表示機能

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,7 +1,10 @@
 class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
-  belongs_to :category, :state, :delivery_charge, :prefecture, :delivery_date
-
+  belongs_to :category
+  belongs_to :state
+  belongs_to :delivery_charge
+  belongs_to :prefecture
+  belongs_to :delivery_date
   belongs_to :user
   has_one_attached :image
 
