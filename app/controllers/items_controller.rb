d9b7593c2cff67587b32342fd74c95fdafead7f9
shotaro0522 <68640527+shotaro0522@users.noreply.github.com>
2022-02-11 10:51:42 +0900

    ログイン状態の場合でも、自身が出品した売却済み商品の商品情報編集ページへ遷移しようとすると、トップページに遷移するよう修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -25,7 +25,7 @@ class ItemsController < ApplicationController
   def edit
     if current_user.id != @item.user.id
       redirect_to root_path
-    elsif Order.exists?(params[:id])
+    elsif Order.exists?(item_id: @item.id)
       redirect_to root_path
     end
   end
